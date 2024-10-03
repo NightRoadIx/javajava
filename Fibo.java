28c29ed1427f3cd8c2740b540480fb6fe49b5be8
NightRoadIx <quintanarxbio@gmail.com>
2024-10-03 02:42:25 -0600

    Update Fibo.java
    Se  genera la clase Fibonacci para calcular y mostrar la serie, así como una forma de asegurar que sólo se reciban números enteros positivos para el cálculo

--- Fibo.java
+++ Fibo.java
@@ -1,48 +1,80 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * La clase Fibonacci se encarga de generar y almacenar una secuencia de números de Fibonacci.
+ * Inicia la secuencia con los valores 0 y 1 por omisión y permite agregar más valores a la secuencia.
+ */
+class Fibonacci {
+    private ArrayList<Integer> fibo; // Almacena los números de la serie de Fibonacci
+
+    /**
+     * Constructor de la clase Fibonacci.
+     * Inicializa el ArrayList y añade los dos primeros números de la serie de Fibonacci: 0 y 1.
+     */
+    public Fibonacci() {
+        fibo = new ArrayList<Integer>();
+        fibo.add(0);  // Añadir el primer número de la serie
+        fibo.add(1);  // Añadir el segundo número de la serie
+    }
+
+    /**
+     * Añade los siguientes números de la serie de Fibonacci hasta completar el tamaño n.
+     * La serie se basa en la suma de los dos números anteriores.
+     *
+     * @param n El número total de elementos que debe tener la serie de Fibonacci.
+     */
+    public void addNumbers(int n) {
+        for (int j = 2; j < n; j++) {
+            fibo.add(fibo.get(j - 1) + fibo.get(j - 2));
+        }
+    }
+
+    /**
+     * Muestra los primeros n números de la serie de Fibonacci almacenados en el ArrayList.
+     *
+     * @param n El número de elementos de la serie de Fibonacci que se desea mostrar.
+     */
+    public void showSeries(int n) {
+        System.out.println("Serie de Fibonacci con " + n + " elementos:");
+        for (int k = 0; k < n; k++) {
+            System.out.print(fibo.get(k));
+            if (k < n - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();  // Para dejar un salto de línea al final
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
-        // Colocar una variable como hardcode
-        // TODO: Agregar una entrada de datos con restricciones
-        // int n = 30;
         Scanner felipe = new Scanner(System.in);
+        int n = -1;  // Inicializar n con un valor inválido para iniciar el bucle
 
-        System.out.println("Ingrésamelo... el número también: ");
-        String jelipeii = felipe.nextLine();
-        for(char tmp : jelipeii.toCharArray())
-        {
-            Character.isDigit(tmp);
-            float jelipeiii = (float)3;
+        // Solicitar al usuario que ingrese un número hasta que ingrese un valor válido (entero positivo)
+        while (n <= 0) {
+            System.out.println("Ingrésamelo... el número también: ");
+            // Verificar si el valor ingresado es un número entero
+            if (felipe.hasNextInt()) {
+                n = felipe.nextInt();
+                if (n <= 0) {
+                    System.out.println("El número debe ser mayor a 0. Intenta de nuevo.");
+                }
+            }
+            else {
+                // Si no es un número entero, limpiar el buffer y mostrar un mensaje de error
+                System.out.println("Eso no es un número entero. Qué te pasa?");
+                felipe.next();  // Limpiar el valor inválido del Scanner
+            }
         }
-        int n;
+        // Crear un objeto de la clase Fibonacci
+        Fibonacci fibonacci = new Fibonacci();
 
-        // Declarar la variable de la serie
-        int serie[] = new int[n];
-        // También se puede declarar un objeto arreglo
-        ArrayList<Integer> fibo = new ArrayList<Integer>();
+        // Añadir los números a la serie
+        fibonacci.addNumbers(n);
 
-        // Estos valores son los por defecto de la serie
-        /*serie[0] = 0; serie[1] = 1;
-        // Calcular los elementos de la serie
-        for(int i = 2; i < n; serie[i] = serie[i-1] + serie[i-2], i++);
-            
-        // Mostrar los datos de la serie ya calculada
-        for(int tmp :serie)
-            System.out.print(tmp + ", ");
-        */
-        fibo.add(0); fibo.add(1);
-        for(int j = 2; j < n; j++)
-            fibo.add(fibo.get(j-1) + fibo.get(j-2));
-
-        // Se puede mostrar toda la serie
-        for(int tmp :fibo)
-            System.out.print(tmp + ", ");
-        // Colocar un salto de línea
-        System.out.println("");
-        // O solamente mostrar lo que pide
-        // el usuario
-        for(int k = 0; k < n; k++)
-            System.out.print(fibo.get(k) + ", ");
+        // Mostrar la serie de Fibonacci
+        fibonacci.showSeries(n);
     }
 }
